abf6e5ef9f5d75e64f250bd5fc12b3dd3dbe32e3
Arthur Botelho <emilioarthur19@gmail.com>
2024-02-24 12:15:05 -0300

    Inserindo Imagem da HomePage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-
 import './App.css';
+import HomePage from './components/HomePage'
 
 function App() {
   return (
@@ -7,6 +7,7 @@ function App() {
        <h1 className="text-3xl font-bold underline">
         Hello world!
         </h1>
+        <HomePage />
     </div>
   );
 }
---
+++ src/components/HomePage/index.jsx
@@ -0,0 +1,11 @@
+import Arthur from "../../images/photos/"
+
+function HomePage() {
+    return (
+        <div>
+            <img src={Arthur}></img>
+        </div>
+    )
+}
+
+export default HomePage
